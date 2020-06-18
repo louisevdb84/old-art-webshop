c89ee906c61549dd0c806bcd40770778695bbbc4
Louise van den Berg <louisevdb84@gmail.com>
2020-06-18 17:02:03 +0200

    Updates

---
+++ precache-manifest.ad887a83cec8857dccb4abfb5b8d4e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b2fd954ee559ef406701127f386012",
+    "url": "/art/index.html"
+  },
+  {
+    "revision": "7291165539b9903378bf",
+    "url": "/art/static/css/2.ffcb96bc.chunk.css"
+  },
+  {
+    "revision": "ab6888f999620404056a",
+    "url": "/art/static/css/main.caffc773.chunk.css"
+  },
+  {
+    "revision": "7291165539b9903378bf",
+    "url": "/art/static/js/2.411cb9c7.chunk.js"
+  },
+  {
+    "revision": "ab6888f999620404056a",
+    "url": "/art/static/js/main.23fcca58.chunk.js"
+  },
+  {
+    "revision": "9cc77ad1409632584eb9",
+    "url": "/art/static/js/runtime~main.97f0e2a9.js"
+  }
+]);
